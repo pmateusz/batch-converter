f64a41b60dc0a3f24299dbbb4fd8af0a338b028b
pmateusz <mat.polnik@gmail.com>
2019-03-03 16:25:41 +0000

    Update copyright information

--- batch_converter.py
+++ batch_converter.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Copyright 2018 Mateusz Polnik
+# Copyright 2019 Mateusz Polnik
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
